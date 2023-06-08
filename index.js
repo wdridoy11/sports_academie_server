78c13081ed981e3627f629765a718008e0ddeae6
Ridoy Sharif <developersridoy@gmail.com>
2023-06-08 20:10:36 +0600

    add classes post method added complete

--- index.js
+++ index.js
@@ -24,7 +24,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
+
+    const academieInstructorsCollection = client.db("sports_academies").collection("academies_instructors");
+    const classeCollection = client.db("sports_academies").collection("classe");
+    
+    // academie Instructors data get
+    app.get("/instructors",async(req,res)=>{
+        const result = await academieInstructorsCollection.find().toArray();
+        res.send(result);
+    })
+    
+  // academie Instructors create class 
+  app.post('/add_classes',async(req,res)=>{
+    const body = req.body;
+    const result = await classeCollection.insertOne(body);
+    res.send(result)
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
