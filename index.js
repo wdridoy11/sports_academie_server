2193589cc339df3f14c05de9c2123d95e1ce52ca
Ridoy Sharif <developersridoy@gmail.com>
2023-06-09 15:26:50 +0600

    academie users admin added

--- index.js
+++ index.js
@@ -56,8 +56,19 @@ async function run() {
       res.send(result)
     })
 
-    // academie users set data
-    
+    // academie users admin
+    app.patch("/users/admin/:id",async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role:"admin"
+        }
+      }
+      const result = await classeCollection.updateOne(filter,updateDoc)
+    res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
